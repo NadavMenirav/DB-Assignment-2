bd126bc2fdf9ed509e0d99bad351a66b595a203f
Nadav Menirav <nmenirav@gmail.com>
2025-12-30 00:13:31 +0200

    (Feat) Returning the total amount spent for each cusomter

--- q7.py
+++ q7.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         host="localhost",
         user="root",
         password="root",
-        database="## PUT THE CORRECT DATABASE HERE IF NEEDED ##",
+        database="biu_shoes",
         port='3307',
     )
 
@@ -14,8 +14,15 @@ if __name__ == '__main__':
     cursor = mydb.cursor()
 
     # Execute the SQL query
+    # For each customer, we return their first name, last name, and the total amount of money he spent (can be zero)
+    # Using LEFT JOIN for the case where a customer has not made any purchases and coalesce to return 0 in that case
     cursor.execute("""
-        ## PUT YOUR QUERY HERE ##
+        SELECT c.first_name first_name, c.last_name last_name, COALESCE(SUM(s.price), 0) total_amount_spent
+        FROM customer c
+         LEFT JOIN order_customer oc ON c.customer_id = oc.customer_id 
+         LEFT JOIN order_shoe os ON oc.order_id = os.order_id 
+         LEFT JOIN shoe s ON os.shoe_id = s.shoe_id
+        GROUP BY c.customer_id;
     """)
 
     # Fetch and print all results separated by commas
