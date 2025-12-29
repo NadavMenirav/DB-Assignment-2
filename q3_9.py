7a929a2847f29bf6aa6a157f3cec0016bfb75168
Nadav Menirav <nmenirav@gmail.com>
2025-12-29 23:09:40 +0200

    (Feat) Populating the shoe_order table

---
+++ q3_9.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    # Execute the SQL query
+    # Using INSERT INTO to populate the order_shoe table using the data given in the assignment pdf
+    cursor.execute("""
+        INSERT INTO order_shoe (order_id, shoe_id)
+        VALUES 
+                (1, 1),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+                (4, 5),
+                (5, 6),
+                (6, 7),
+                (7, 8),
+                (8, 9),
+                (9, 10);
+    """)
+
+    # !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    # Close the cursor and the connection
+    cursor.close()
+    mydb.close()
