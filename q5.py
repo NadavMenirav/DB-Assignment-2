fa27389e7bfd9ca67ecc188b0d1f26a401e53334
Nadav Menirav <nmenirav@gmail.com>
2025-12-29 23:29:05 +0200

    (Feat) Adding the pre_order_availabe column to upcoming table

---
+++ q5.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    # Execute the SQL query
+    # Using the ALTER TABLE command to add the pre_order_available column (which is a single bit value which
+    # is 0 by default) to the upcoming table
+    cursor.execute("""
+        ALTER TABLE upcoming
+        ADD pre_order_available BIT DEFAULT 0;
+    """)
+
+    # !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    # Close the cursor and the connection
+    cursor.close()
+    mydb.close()
